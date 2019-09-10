3e5f27a13430d27e7b880e2010b0c5f713674aec
Bibil Mathew Chacko <bibil@bestdocapp.com>
2019-09-10 17:41:12 +0530

    first commit

---
+++ src/main/java/thermalrock/ThermalrockApplication.java
@@ -0,0 +1,13 @@
+package thermalrock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThermalrockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThermalrockApplication.class, args);
+	}
+
+}
